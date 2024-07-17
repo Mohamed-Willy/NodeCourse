b21fd80a9456431a43513d0588ee20fd8200d86c
Mohamed Willy <mohamedwilly2001@gmail.com>
2024-07-17 15:08:48 +0300

    Day 2
    Until P2-V9

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,48 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-const hello = 'Hello World'
-console.log(hello);
+// Blocking, Synchronous Way
+/*
+    // Read From File
+    const inputText = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
+    console.log(inputText);
+    // Write Into File
+    const outputText = `I am an amazing programmer \n\n\n ${inputText}`
+    fs.writeFileSync('./starter/txt/output.txt', outputText);
+*/
+// Non-blocking, Asynchronous Way
+/*
+    //Read From File
+    fs.readFile('./starter/txt/input.txt', 'utf-8', (err, data) => {
+        console.log(data);
+        //Read From File
+        fs.writeFile('./starter/txt/AsyncOutput', `Willyyyyyyyy \n\n\n ${data} \n Got Data`,(err) => {
+            console.log("Finished Writing");
+        });
+        console.log("Writing Data");
+    })
+    console.log("Reading Data");
+*/
 
+// Creating the server
+const server = http.createServer((request, response) =>{
+    // console.log(`Request URL: ${request.url}`)
+    const pathName = request.url;
+    if(pathName === '/home' || pathName === '/'){
+        response.end("This is the home page");
+    }
+    else if(pathName === '/product'){
+        response.end("This is the product page");
+    }
+    else {
+        response.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'Willy Error Header'
+        });
+        response.end('<h1>Hello I\'m the server you are falling back</h1>');
+    }
+});
+server.listen(8000, '127.0.0.1', () =>{
+    console.log('Listening on 127.0.0.1 port 8000');
+});
--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Notes:
     1- Ctrl + ` -> to open terminal
     2- Write {node} to enter the REPL and {Ctrl + D} to Exit
     3- To make a shortcut in terminal {File -> Preferences -> Keyboard Shortcuts} Clear Terminal comand -> workbench.action.terminal.clear
-    4-
+    4- Callback Hell ![alt text](image.png)
+    5- Use {CTRL + C} to exit the process (Server)
