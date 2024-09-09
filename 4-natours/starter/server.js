1416280db14c2ae87b98f4fd1832bea29717cf65
Mohamed Willy <mohamedwilly2001@gmail.com>
2024-09-09 09:58:11 +0300

    Day 11
    Until P8-V5

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,9 +1,42 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose.connect(DB).then(() => console.log('Connected to MongoDB...'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    required: [true, 'A tour price must have a value'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+testTour
+  .save()
+  .then((doc) => {
+    console.log('New tour saved: ', doc);
+  })
+  .catch((err) => {
+    console.log('Error saving tour: ', err);
+  });
+
 app.listen(process.env.PORT, () => {
   console.log('listening on port ', process.env.PORT, '...');
 });
